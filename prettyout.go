b637d07c1c545e207881427e1ae6ae29dbc8f8f8
WalkerSumida <walkersumida@gmail.com>
2022-01-09 18:36:55 +0900

    feat: change output format

--- prettyout.go
+++ prettyout.go
@@ -8,6 +8,6 @@ import (
 
 func Error(t *testing.T, expected, got interface{}, opts ...cmp.Option) {
 	if diff := cmp.Diff(expected, got, opts...); diff != "" {
-		t.Errorf("-expected +got: %s", diff)
+		t.Errorf("\n-expected +got: \n%s", diff)
 	}
 }
